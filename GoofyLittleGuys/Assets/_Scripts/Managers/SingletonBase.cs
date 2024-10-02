d8f8e63dac3217a50d86a66fe218a50d3dca9fda
DprssdChckn <tberner111@gmail.com>
2024-10-02 18:14:48 -0400

    Setup
    -lil guy base (needs changes)
    -slight file restructure
    -singleton base class for managers

---
+++ GoofyLittleGuys/Assets/_Scripts/Managers/SingletonBase.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Managers
+{
+    public class SingletonBase<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance;
+
+        [SerializeField] private bool dontDestroyOnLoad = true;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+                Destroy(this.gameObject);
+
+            else if (Instance == null)
+                Instance = this as T;
+        }
+
+        public void Ping()
+        {
+            Debug.Log("Pinged " + this.GetType().Name);
+        }
+    }
+}
