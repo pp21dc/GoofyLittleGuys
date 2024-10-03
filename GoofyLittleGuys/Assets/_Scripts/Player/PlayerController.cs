901a4862e100f01c92b529420f870e87a5604a8d
romank <xt19uj@brocku.ca>
2024-10-02 22:47:54 -0400

    Player Input and Input System
    - Added Input System to project
    - Setup Input Action Map for UI and Open World controls
    - Created PlayerController script to be hooked up to Player Input component

---
+++ GoofyLittleGuys/Assets/_Scripts/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+	public void OnMove(InputAction.CallbackContext ctx)
+	{
+
+	}
+	public void OnSwap(InputAction.CallbackContext ctx)
+	{
+
+	}	
+	public void OnPause(InputAction.CallbackContext ctx)
+	{
+
+	}
+	public void OnJump(InputAction.CallbackContext ctx)
+	{
+
+	}
+	public void OnPrimarySkill(InputAction.CallbackContext ctx)
+	{
+
+	}
+	public void OnSecondarySkill(InputAction.CallbackContext ctx)
+	{
+
+	}
+}
