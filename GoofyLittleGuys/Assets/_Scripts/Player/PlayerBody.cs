3231ee31662ba69c8d6d906033d62eb73e794305
romank <xt19uj@brocku.ca>
2024-10-03 08:38:51 -0400

    Added Swap Input and Player Jump Adjustments
    - Added the ability to swap between lil guys in the player's team
    - Adjusted jumping to not allow jumping if the player isn't grounded
    - Added Ground Layer to project

--- GoofyLittleGuys/Assets/_Scripts/Player/PlayerBody.cs
+++ GoofyLittleGuys/Assets/_Scripts/Player/PlayerBody.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class PlayerBody : MonoBehaviour
 {
 	[SerializeField]
+	private LayerMask groundLayer;
+	[SerializeField]
 	private List<LilGuyBase> lilGuyTeam;
 	[SerializeField]
 	private float fallMultiplier = 2.5f;
@@ -13,21 +15,47 @@ public class PlayerBody : MonoBehaviour
 	private float lowJumpMultiplier = 2f;
 	[SerializeField, Range(1, 10)]
 	private float jumpSpeed = 2f;
+	[SerializeField, Range(1, 10)]
+	private float maxSpeed = 10f; // To be replaced with lilGuys[0].speed
 	[SerializeField]
 	private Vector3 movementDirection = Vector3.zero;
 	private Rigidbody rb;
-	[SerializeField, ReadOnly]
-	private Vector3 rbVelocity;
 	public bool isJumping = false;
 
 	public void UpdateMovementVector(Vector2 dir)
 	{
 		movementDirection = new Vector3(dir.x, 0, dir.y);
 	}
-	public void JumpPerformed(bool endJumpEarly)
+	public void JumpPerformed(double durationHeld)
 	{
-		isJumping = !endJumpEarly;
+		if (!IsGrounded()) return;
 		rb.velocity += Vector3.up * jumpSpeed;
+	}
+	public void SwapLilGuy(float shiftDirection)
+	{
+		if (lilGuyTeam.Count <= 1) return;
+		if (shiftDirection > 0)
+		{
+			LilGuyBase currentSelected = lilGuyTeam[0];
+			for (int i = 1; i < lilGuyTeam.Count - 1; i++)
+			{
+				lilGuyTeam[i] = lilGuyTeam[i + 1];
+			}
+			lilGuyTeam[lilGuyTeam.Count - 1] = currentSelected;
+		}
+		else
+		{
+			LilGuyBase lastInTeam = lilGuyTeam[lilGuyTeam.Count - 1];
+			for (int i = lilGuyTeam.Count - 1; i > 0; i++)
+			{
+				lilGuyTeam[i] = lilGuyTeam[i - 1];
+			}
+			lilGuyTeam[0] = lastInTeam;
+		}
+	}
+	private bool IsGrounded()
+	{
+		return Physics.Raycast(transform.position - Vector3.down * 0.1f, Vector3.down, 0.1f, groundLayer);
 	}
 	private void Start()
 	{
@@ -35,10 +63,9 @@ public class PlayerBody : MonoBehaviour
 	}
 	private void FixedUpdate()
 	{
-		rbVelocity = rb.velocity;
-		Vector3 newForceDirection = movementDirection * 100 * Time.fixedDeltaTime;
-		rb.velocity += newForceDirection;
-
+		Vector3 targetVelocity = movementDirection * maxSpeed;
+		Vector3 newForceDirection = (targetVelocity - new Vector3(rb.velocity.x, 0, rb.velocity.z));
+		rb.velocity += newForceDirection * Time.fixedDeltaTime;
 		if (rb.velocity.y < 0)
 		{
 			rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
--- GoofyLittleGuys/Assets/_Scripts/Player/PlayerController.cs
+++ GoofyLittleGuys/Assets/_Scripts/Player/PlayerController.cs
@@ -5,14 +5,14 @@ public class PlayerController : MonoBehaviour
 {
 	[SerializeField]
 	private PlayerBody playerBody;
-
+	
 	public void OnMove(InputAction.CallbackContext ctx)
 	{
 		playerBody.UpdateMovementVector(ctx.ReadValue<Vector2>());
 	}
 	public void OnSwap(InputAction.CallbackContext ctx)
 	{
-
+		playerBody.SwapLilGuy(ctx.ReadValue<float>());
 	}
 	public void OnPause(InputAction.CallbackContext ctx)
 	{
@@ -20,14 +20,9 @@ public class PlayerController : MonoBehaviour
 	}
 	public void OnJump(InputAction.CallbackContext ctx)
 	{
-		if (ctx.canceled)
-		{
-			playerBody.JumpPerformed(true);
-		}
-		else
-		{
-			playerBody.JumpPerformed(false);
-		}
+		Debug.Log(ctx.duration);
+		playerBody.JumpPerformed(ctx.duration);
+
 	}
 	public void OnPrimarySkill(InputAction.CallbackContext ctx)
 	{
