544b283bc49174f0175475286b2930c2ef3090b1
romank <xt19uj@brocku.ca>
2024-10-11 15:46:38 -0400

    Defense Type Special Implemented
    - Implemented shield special with currently one editable variable that varies between lil guys (other 2 variables currently undecided)
    - Added Shield.cs script which would be on the shield GO that is instantiated when the special is used.

--- GoofyLittleGuys/Assets/_Scripts/LilGuys/Base/DefenseType.cs
+++ GoofyLittleGuys/Assets/_Scripts/LilGuys/Base/DefenseType.cs
@@ -3,14 +3,23 @@ using UnityEngine;
 public class DefenseType : LilGuyBase
 {
     private Transform attackPos;
+    private GameObject spawnedShieldObj = null;    // The actual instantiated shield object on the lil guy
 
-    public DefenseType(string guyName, int heath, int maxHealth, PrimaryType type, int speed, int stamina, int strength) : base(guyName, heath, maxHealth, type, speed, stamina, strength)
+    [SerializeField] private GameObject shieldPrefab; // The shield prefab to instantiate
+    [SerializeField] private float duration = 1;
+	// Insert variable 2 and 3 that can vary between lil guys
+	public GameObject SpawnedShieldObj { get { return spawnedShieldObj; } set { spawnedShieldObj = value; } }
+
+	public DefenseType(string guyName, int heath, int maxHealth, PrimaryType type, int speed, int stamina, int strength) : base(guyName, heath, maxHealth, type, speed, stamina, strength)
     {
     }
 
     public override void Special()
-    {
-        //TODO: ADD DEFENSE SPECIAL ATTACK
-        base.Special();
+	{
+		//TODO: ADD DEFENSE SPECIAL ATTACK
+		if (cooldownTimer > 0 || currentCharges <= 0) return;
+		spawnedShieldObj ??= Instantiate(shieldPrefab, transform.position, Quaternion.identity, transform); // If spawnShieldObj is null, assign it this instantiated GO
+        spawnedShieldObj.GetComponent<Shield>().Initialize(duration);
+
     }
 }
---
+++ GoofyLittleGuys/Assets/_Scripts/Misc/Shield.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    private float shieldDuration = 1;
+    private float shieldTimer = 0;
+    [SerializeField] private int shieldHealth = 1;
+    [SerializeField] private float maxSize = 1;
+    [SerializeField] private float expansionSpeed = 1;
+    public int ShieldHealth {  get { return shieldHealth; } set { shieldHealth = value; } }
+
+        
+    public void Initialize(float duration)
+    {
+        shieldDuration = duration;
+        StartCoroutine("ShieldUp");
+    }
+
+	private void Start()
+	{
+        transform.localScale = Vector3.zero;
+        StartCoroutine(ResizeShieldOvertime(Vector3.zero, Vector3.one * maxSize));
+	}
+	private void OnDestroy()
+	{
+        StartCoroutine(ResizeShieldOvertime(Vector3.one * maxSize, Vector3.zero));
+	}
+
+	private IEnumerator ShieldUp()
+    {
+        shieldTimer = shieldDuration;
+        while (shieldTimer > 0)
+        {
+            if (shieldHealth <= 0) break;
+            shieldTimer -= Time.deltaTime;
+            yield return null;
+        }
+        transform.parent.GetComponent<DefenseType>().SpawnedShieldObj = null;
+        Destroy(gameObject);
+    }
+    
+    private IEnumerator ResizeShieldOvertime(Vector3 initialScale, Vector3 targetScale)
+    {
+		float elapsedTime = 0;
+		while (elapsedTime < expansionSpeed)
+		{
+			// Expanding the hitbox zone
+			transform.localScale = Vector3.Lerp(initialScale, targetScale, elapsedTime / expansionSpeed);
+			elapsedTime += Time.deltaTime;
+
+			yield return null;
+		}
+	}
+
+}
