8e94c421c163a0e099fe01d4606ccdeef0a4138c
DprssdChckn <tberner111@gmail.com>
2024-10-15 16:13:31 -0400

    hitbox/hurtbox
    forgot to push this earlier + some changes to layer detection

---
+++ GoofyLittleGuys/Assets/_Scripts/Misc/Hitbox.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Auth: Thomas Berner
+// - Hitbox class for any combat based trigger
+// -> can be derrived from for other attacks which provide knockback or other effects
+public class Hitbox : MonoBehaviour
+{
+    protected int Damage;
+    public LayerMask layerMask;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (layerMask == (layerMask | (1 << other.transform.gameObject.layer)))
+        {
+            Hurtbox h = other.GetComponent<Hurtbox>();
+
+            if (h != null)
+                OnHit(h);
+        }
+    }
+
+    private void OnHit(Hurtbox h)
+    {
+        h.TakeDamage(Damage);
+        Destroy(this.gameObject);
+    }
+
+}
---
+++ GoofyLittleGuys/Assets/_Scripts/Misc/Hurtbox.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+
+//Auth: Thomas Berner
+// - add as a component to detect any damage and keep track of health, keeps all combat collisions on a single layer
+public class Hurtbox : MonoBehaviour
+{
+    [SerializeField] private int health;
+    private bool lilGuy;
+
+    private void Awake()
+    {
+        if (gameObject.GetComponentInParent<PlayerBody>() != null)
+        {
+            health = gameObject.GetComponentInParent<PlayerBody>().;
+            lilGuy = true;
+        }
+        else
+        {
+            lilGuy = false;
+        }
+    }
+
+    /// <summary>
+    /// Gets the health value of given object. To be called when a lil guy is damaged.
+    /// </summary>
+    public void TakeDamage(int dmg)
+    {
+        if (lilGuy)
+        {
+            gameObject.GetComponent<LilGuyBase>().health -= dmg;
+            health = gameObject.GetComponent<LilGuyBase>().health;
+        }
+        else
+        {
+            gameObject.GetComponent<Hurtbox>().health -= dmg;
+        }
+    }
+
+    //
+}
