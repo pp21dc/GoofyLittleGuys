eb68dda17fb90ad8dd65230ee0e18ca6619939f0
romank <xt19uj@brocku.ca>
2024-10-18 10:55:07 -0400

    Event Manager Implementation
    - Event Manager class created with all necessary events and method calls to invoke said events, as described on the whiteboard :3c

---
+++ GoofyLittleGuys/Assets/_Scripts/Managers/EventManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager
+{
+	public delegate void LilGuyLastHitDelegate(PlayerBody body);
+	public event LilGuyLastHitDelegate NotifyLastHit;
+
+	public delegate void LegendarySpawnedDelegate();
+	public event LegendarySpawnedDelegate NotifyLegendarySpawned;
+
+	public delegate void ChangePhaseDelegate();
+	public event ChangePhaseDelegate NotifyPhaseChanged;
+
+	public delegate void StatsChangeDelegate(LilGuyBase statsToChange);
+	public event StatsChangeDelegate NotifyStatsChanged;
+
+	public delegate void GameOverStateDelegate();
+	public event GameOverStateDelegate NotifyGameOver;
+
+	public delegate void MicrogameFailedDelegate(PlayerBody body);
+	public event MicrogameFailedDelegate NotifyMicrogameFailed;
+
+	private static EventManager _instance = null;
+    public static EventManager Instance
+    {
+        get
+        {
+			if (_instance == null) _instance = new EventManager();
+			return _instance;
+		}
+    }
+
+	public void CallLilGuyLastHitEvent(PlayerBody body)
+	{
+		NotifyLastHit?.Invoke(body);
+	}
+
+	public void CallLegendarySpawnedEvent()
+	{
+		NotifyLegendarySpawned?.Invoke();
+	}
+
+	public void CallPhaseChangedEvent()
+	{
+		NotifyPhaseChanged?.Invoke();
+	}
+
+	public void CallStatsChangedEvent(LilGuyBase statsToChange)
+	{
+		NotifyStatsChanged?.Invoke(statsToChange);
+	}
+
+	public void CallGameOverEvent()
+	{
+		NotifyGameOver?.Invoke();
+	}
+
+	public void CallMicrogameFailedEvent(PlayerBody body)
+	{
+		NotifyMicrogameFailed?.Invoke(body);
+	}
+
+
+}
