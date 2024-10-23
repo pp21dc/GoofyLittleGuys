cde5ad7428c24228b3a2288ea25e26560935f1bb
romank <xt19uj@brocku.ca>
2024-10-22 20:18:56 -0400

    Persistent Scene Setup + Level Loading
    - Setup persistent scene with GameManager, Multiplayerhandler and LevelLoadManager
    - Made basic level load manager that will need to be updated and iterated upon

---
+++ GoofyLittleGuys/Assets/_Scripts/Managers/LevelLoadManager.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Managers
+{
+	public class LevelLoadManager : SingletonBase<LevelLoadManager>
+	{
+		public static LevelLoadManager _Instance;
+		[SerializeField]
+		private GameObject loadingScreen;
+
+		[SerializeField]
+		private string[] levelNames;
+		public string[] LevelNames { get { return levelNames; } }
+
+		private string currLevelName;
+		[SerializeField]
+		private bool isLoadingLevel = false;
+
+		[SerializeField]
+		private List<string> currentLevelList;
+		private WaitForSeconds bufferTime;
+
+		private void Start()
+		{
+			LoadLevel(levelNames[1]);
+			SceneManager.SetActiveScene(SceneManager.GetSceneByName("-01_PersistentScene"));
+		}
+
+		private IEnumerator LoadLevel(string levelName)
+		{
+			isLoadingLevel = true;
+			Time.timeScale = 1;
+			yield return bufferTime;
+
+			// Unload all opened scenes (Not the persistent scene
+			if (currentLevelList.Count != 0)
+			{
+				for (int i = 0; i < currentLevelList.Count; i++)
+					SceneManager.UnloadSceneAsync(currentLevelList[i]);
+				currentLevelList.Clear();
+			}
+
+			// Load new scene
+			AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
+
+			while (!asyncLoad.isDone)
+			{
+				// If we have a loading screen, we can update the slider progress, and pass asyncLoad.progress
+				yield return null;
+			}
+			SceneManager.SetActiveScene(SceneManager.GetSceneByName(levelName));
+
+			currentLevelList.Add(levelName);
+			// Initialize player etc.
+			yield return bufferTime;
+
+			loadingScreen.SetActive(false);
+			isLoadingLevel = false;
+
+
+			yield break;
+		}
+	}
+}
