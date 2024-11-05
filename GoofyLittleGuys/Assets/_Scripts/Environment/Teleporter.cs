71e3014adb793a54d56d69fb797097ea67cf34fc
DprssdChckn <tberner111@gmail.com>
2024-11-05 17:12:25 -0500

    teleporter script

---
+++ GoofyLittleGuys/Assets/_Scripts/Environment/Teleporter.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Teleporter : MonoBehaviour
+{
+    // -- Variables --
+    [SerializeField] private float timeToTeleport;
+    [SerializeField] private Teleporter targetTeleporter;
+    private float timer = 0.0f;
+    private BoxCollider collider;
+
+    private void Start()
+    {
+        collider = GetComponent<BoxCollider>();
+        collider.isTrigger = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            timer += Time.deltaTime;
+            Debug.Log(timer);
+            if (timer >= timeToTeleport)
+            {
+                if (targetTeleporter != null)
+                    other.gameObject.transform.position = targetTeleporter.transform.position;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            timer = 0.0f;
+    }
+}
