537b35c9a69cbf803e73f9239854eb723dc40d44
romank <xt19uj@brocku.ca>
2024-11-08 19:26:23 -0500

    Last Hit Prompt/Character Select Menu Bug Fixes
    - Fixed the last hit prompt menu from scaling weird in split screen
    - Fixed a bug with the buttons not highlighting in red as intended
    - Added functionality in Character Select Menu to undo your lil guy selection by pressing the cancel input (Button South or \ on Keyboard)
    - Moved the scripts that were outside in the Assets root folder into their respective place in the Scripts folder

--- GoofyLittleGuys/Assets/_Scripts/Ui/CharacterSelectMenu.cs
+++ GoofyLittleGuys/Assets/_Scripts/Ui/CharacterSelectMenu.cs
@@ -1,24 +1,43 @@
+using Managers;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
 using UnityEngine.UI;
 
 public class CharacterSelectMenu : MonoBehaviour
 {
+	public enum CharacterSelectState
+	{
+		CharacterSelect,
+		LockedIn,
+		Confirmed
+	}
+
+
+	[SerializeField] private PlayerInput player;
+	[SerializeField] private MultiplayerEventSystem playerEventSystem;
 	[SerializeField] private List<Button> buttons;
 	public List<LilGuyBase> starters;
 	[SerializeField] private PlayerBody body;
 	bool lockedIn = false;
+	private CharacterSelectState currentState;
+
 	public bool LockedIn { get { return lockedIn; } }
+
 
 	private void Start()
 	{
+		player.actions["Cancel"].performed += OnCancelled;
+		player.actions["Submit"].performed += OnSubmitted;
 		EventManager.Instance.GameStarted += GameInit;
 	}
 
 	private void OnDestroy()
 	{
+		player.actions["Cancel"].performed -= OnCancelled;
+		player.actions["Submit"].performed -= OnSubmitted;
 		EventManager.Instance.GameStarted -= GameInit;
 	}
 
@@ -26,25 +45,71 @@ public class CharacterSelectMenu : MonoBehaviour
 	{
 		gameObject.SetActive(false);
 	}
+	private void OnSubmitted(InputAction.CallbackContext ctx)
+	{
+		if (currentState == CharacterSelectState.LockedIn && CheckIfValidGameStart())
+		{
+			EventManager.Instance.CallLilGuyLockedInEvent();
+		}
+	}
+	private void OnCancelled(InputAction.CallbackContext ctx)
+	{
+		switch (currentState)
+		{
+			case CharacterSelectState.CharacterSelect:
+				foreach (PlayerInput input in PlayerInput.all)
+				{
+					Destroy(input.gameObject);
+				}
+				LevelLoadManager.Instance.LoadNewLevel("00_MainMenu");
+				break;
+			case CharacterSelectState.LockedIn:
+				currentState = CharacterSelectState.CharacterSelect;
+				lockedIn = false;
+				GameObject lilGuyToRemove = body.LilGuyTeam[0].gameObject;
+				body.LilGuyTeam.RemoveAt(0);
+				Destroy(lilGuyToRemove);
+				SetButtonLockState(false);
+				playerEventSystem.SetSelectedGameObject(buttons[0].gameObject);
+				break;
+			case CharacterSelectState.Confirmed:
+				currentState = CharacterSelectState.LockedIn;
+				break;
 
+		}
+	}
+
+	private bool CheckIfValidGameStart()
+	{
+		if (PlayerInput.all.Count < 2) return false;
+		foreach (PlayerInput input in PlayerInput.all)
+		{
+			if (!input.GetComponentInChildren<CharacterSelectMenu>().LockedIn) return false;
+		}
+		return true;
+	}
+
+	private void SetButtonLockState(bool lockState)
+	{
+		foreach (Button button in buttons)
+		{
+			button.interactable = !lockState;
+		}
+	}
 	public void OnLockedIn(int choice)
 	{
 		lockedIn = true;
-		foreach (Button button in buttons)
-		{
-			button.interactable = false;
-		}
+		currentState = CharacterSelectState.LockedIn;
+		SetButtonLockState(true);
 		GameObject starter = Instantiate(starters[choice].gameObject);
 		starter.GetComponent<LilGuyBase>().Init(LayerMask.NameToLayer("PlayerLilGuys"));
-
 		starter.transform.SetParent(body.LilGuyTeamSlots[0].transform, false);  // false keeps local position/rotation
 		starter.transform.localPosition = Vector3.zero;  // Ensure it’s positioned at the parent
 		starter.GetComponent<Rigidbody>().isKinematic = true;
 		body.LilGuyTeam.Add(starter.GetComponent<LilGuyBase>());
 		body.LilGuyTeam[0].playerOwner = body.gameObject;
-		EventManager.Instance.CallLilGuyLockedInEvent();
 
-	}	
+	}
 
 	public void OnPlayGameButtonPressed()
 	{
