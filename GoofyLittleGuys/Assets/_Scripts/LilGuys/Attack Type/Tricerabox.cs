bd359661cd098820f6b920f5ea684d98b4d4d2a0
romank <xt19uj@brocku.ca>
2024-11-18 16:08:23 -0500

    Created Tricera-box Prefab
    - Made Tricera-box prefab with script, and some default attack values

---
+++ GoofyLittleGuys/Assets/_Scripts/LilGuys/Attack Type/Tricerabox.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tricerabox : StrengthType
+{
+	public Tricerabox(string guyName, int heath, int maxHealth, PrimaryType type, int speed, int stamina, int strength) : base(guyName, heath, maxHealth, type, speed, stamina, strength)
+	{
+	}
+}
