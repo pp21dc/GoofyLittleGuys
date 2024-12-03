147ee67eac0f391700d550161fdf2104af08db40
Patrick Perry <patrickperry713@gmail.com>
2024-12-03 15:57:46 -0500

    Debug UI For player Stats Implemented

--- GoofyLittleGuys/Assets/_Scripts/Ui/PlayerUi.cs
+++ GoofyLittleGuys/Assets/_Scripts/Ui/PlayerUi.cs
@@ -13,12 +13,22 @@ public class PlayerUi : MonoBehaviour
     [SerializeField] Image persistentIcon;
     [SerializeField] Image persistentAbilityIcon;
     [SerializeField] TMP_Text berryCountText;
-    
-    LilGuyPopout GetLilGuyPopout(int index)                                  //Not 100% when this would be needed - hence it not being implemented yet
-    {
-        throw new NotImplementedException();
-    }
 
+    [SerializeField] PlayerBody pb;
+
+    [SerializeField] TMP_Text STR_Txt;
+    [SerializeField] TMP_Text SPD_Txt;
+    [SerializeField] TMP_Text DEF_Txt;
+    [SerializeField] TMP_Text LVL_Txt;
+    [SerializeField] TMP_Text HP_Txt;
+    private void Update()
+    {
+        STR_Txt.text = "STR: " + pb.LilGuyTeam[0].Strength.ToString();
+        SPD_Txt.text = "SPD: " + pb.LilGuyTeam[0].Speed.ToString();
+        DEF_Txt.text = "DEF: " + pb.LilGuyTeam[0].Defense.ToString();
+        LVL_Txt.text = "LVL: " + pb.LilGuyTeam[0].Level.ToString();
+        HP_Txt.text =  "HP: "  + pb.LilGuyTeam[0].Health.ToString() + " / " + pb.LilGuyTeam[0].MaxHealth.ToString();
+    }
     public void SetBerryCount(int count)
     {
         berryCountText.text = $"{count}/3";
