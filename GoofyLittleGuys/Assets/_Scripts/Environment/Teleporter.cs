4a4c51b6b69cdccd4d237722d7b7fa688698b246
DprssdChckn <78516120+DprssdChckn@users.noreply.github.com>
2025-01-07 20:51:34 -0500

    portal work
    Just need to test more cases in game but working good so far

--- GoofyLittleGuys/Assets/_Scripts/Environment/Teleporter.cs
+++ GoofyLittleGuys/Assets/_Scripts/Environment/Teleporter.cs
@@ -1,37 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider))]
-public class Teleporter : MonoBehaviour
+public class Teleporter : InteractableBase
 {
     // -- Variables --
-    [SerializeField] private float timeToTeleport;
     [SerializeField] private Teleporter targetTeleporter;
-    private float timer = 0.0f;
+    [SerializeField] private Transform endTeleportLocation;
+    [SerializeField] private float cooldown;
+    private bool onCooldown;
     private BoxCollider teleporterCollider;
+    private List<GameObject> inRange = new List<GameObject>();
 
+    // -- Getters --
+    public Transform EndTeleportLocation { get { return endTeleportLocation; } }
+    public bool OnCooldown { get { return onCooldown; } set { onCooldown = value; } }
+
+    #region Event Functions
     private void Start()
     {
         teleporterCollider = GetComponent<BoxCollider>();
         teleporterCollider.isTrigger = true;
+        interactableCanvas.SetActive(false);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 7 && !inRange.Contains(other.gameObject))
+        {
+            inRange.Add(other.gameObject);
+            other.GetComponent<PlayerBody>().ClosestInteractable = this;
+        }
+    }
+
+    // Using this as a cheaper update loop
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.layer == 7)
         {
-            timer += Time.deltaTime;
-            Debug.Log(timer);
-            if (timer >= timeToTeleport)
+            if (inRange.Count > 0 && !onCooldown)
             {
-                if (targetTeleporter != null)
-                    other.gameObject.transform.position = targetTeleporter.transform.position;
+                interactableCanvas.SetActive(true);
+            }
+            else
+            {
+                interactableCanvas.SetActive(false);
             }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
-            timer = 0.0f;
+        if (other.gameObject.layer == 7 && inRange.Contains(other.gameObject))
+        {
+            inRange.Remove(other.gameObject);
+            other.GetComponent<PlayerBody>().ClosestInteractable = null;
+        }
+    }
+    #endregion
+
+    public override void OnInteracted(PlayerBody body)
+    {
+        base.OnInteracted(body);
+
+        body.gameObject.transform.position = targetTeleporter.EndTeleportLocation.position;
+    }
+
+    private IEnumerator WaitForCooldown()
+    {
+        onCooldown = true;
+        targetTeleporter.OnCooldown = true;
+        yield return new WaitForSeconds(cooldown);
+        onCooldown = false;
+        targetTeleporter.OnCooldown = false;
     }
 }
