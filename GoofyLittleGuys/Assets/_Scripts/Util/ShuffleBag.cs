fa3aa9ebb54cd3bcb30b02cbb82290926f70fbac
RomanK <xt19uj@brocku.ca>
2025-02-08 11:44:06 -0500

    Added Comments to ShuffleBag code

--- GoofyLittleGuys/Assets/_Scripts/Util/ShuffleBag.cs
+++ GoofyLittleGuys/Assets/_Scripts/Util/ShuffleBag.cs
@@ -1,29 +1,64 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Util
 {
 	/// <summary>
-	/// Shuffle Bag Algorithm Class. Functions like drawing names out of a hat. It randomizes the sequence of T objects, and then takes T out of the hat. When all T's are removed, then 
-	/// the shuffle bag reshuffles and starts from the beginning again.
+	/// A Shuffle Bag that randomizes the order of elements and cycles through them.
+	/// When all elements are drawn, the bag reshuffles and starts again.
 	/// </summary>
-	/// <typeparam name="T"></typeparam>
-	public class ShuffleBag<T> : ICollection<T>, IList<T>
+	/// <typeparam name="T">The type of elements contained in the shuffle bag.</typeparam>
+	public class ShuffleBag<T>
 	{
-		private List<T> data = new List<T>();
-		private int cursor = 0;
+		private List<T> data = new List<T>(); // Internal list storing elements
+		private int cursor = 0; // Tracks the current position in the shuffled sequence
 
 		/// <summary>
-		/// Gets the next value in the list of values.
+		/// Creates an empty Shuffle Bag.
 		/// </summary>
-		/// <returns>T: The next value in the randomized sequence.</returns>
+		public ShuffleBag() { }
+
+		/// <summary>
+		/// Creates a Shuffle Bag with an initial set of values.
+		/// </summary>
+		/// <param name="initialValues">The initial set of values to populate the shuffle bag.</param>
+		public ShuffleBag(IEnumerable<T> initialValues)
+		{
+			AddRange(initialValues);
+		}
+
+		/// <summary>
+		/// Adds an item to the shuffle bag and reshuffles the contents.
+		/// </summary>
+		/// <param name="item">The item to be added.</param>
+		public void Add(T item)
+		{
+			data.Add(item);
+			Shuffle();
+		}
+
+		/// <summary>
+		/// Adds multiple items to the shuffle bag and reshuffles the contents.
+		/// </summary>
+		/// <param name="collection">A collection of items to add.</param>
+		public void AddRange(IEnumerable<T> collection)
+		{
+			data.AddRange(collection);
+			Shuffle();
+		}
+
+		/// <summary>
+		/// Retrieves the next item in the shuffled sequence. If all items have been drawn, the bag reshuffles and starts again.
+		/// </summary>
+		/// <returns>The next item in the sequence.</returns>
+		/// <exception cref="System.InvalidOperationException">Thrown if the shuffle bag is empty.</exception>
 		public T Next()
 		{
+			if (data.Count == 0)
+				throw new System.InvalidOperationException("ShuffleBag is empty.");
 
 			if (cursor >= data.Count)
 			{
-				// End of list reached, reshuffle and reset cursor back to start.
 				Shuffle();
 				cursor = 0;
 			}
@@ -32,110 +67,29 @@ namespace Util
 		}
 
 		/// <summary>
-		/// Helper method that randomizes the sequence of the values in the data list.
+		/// Randomizes the order of elements in the shuffle bag.
 		/// </summary>
 		private void Shuffle()
 		{
 			for (int i = 0; i < data.Count; i++)
 			{
 				int randomIndex = Random.Range(i, data.Count);
-				T temp = data[i];
-				data[i] = data[randomIndex];
-				data[randomIndex] = temp;
+				(data[i], data[randomIndex]) = (data[randomIndex], data[i]); // Swap elements
 			}
 		}
 
 		/// <summary>
-		/// Constructor that initializes a shuffle bag with a given array of values.
+		/// Clears all elements from the shuffle bag.
 		/// </summary>
-		/// <param name="initialValues"> An array of values to initialize the shuffle bag with.</param>
-		public ShuffleBag(T[] initialValues)
+		public void Clear()
 		{
-			for (int i = 0; i < initialValues.Length; i++) Add(initialValues[i]);
+			data.Clear();
+			cursor = 0;
 		}
 
 		/// <summary>
-		/// Constructor that initializes a shuffle bag with a given list of values.
+		/// Gets the number of items currently in the shuffle bag.
 		/// </summary>
-		/// <param name="initialValues"> An list of values to initialize the shuffle bag with.</param>
-		public ShuffleBag(List<T> initialValues)
-		{
-			for (int i = 0; i < initialValues.Count; i++) Add(initialValues[i]);
-		}
-
-		/// <summary>
-		/// Default Shuffle bag with empty list.
-		/// </summary>
-		public ShuffleBag() { }
-
-		/// <summary>
-		/// Getter/Setter that returns or sets the value of the shuffle bag at the given index.
-		/// </summary>
-		/// <param name="index">The index to set or get from in the shuffle bag.</param>
-		/// <returns>T: The object at the given index.</returns>
-		public T this[int index] { get => data[index]; set => data[index] = value; }
-
-
-		public bool IsReadOnly => false;
-
-		public void Add(T item)
-		{
-			data.Add(item);
-			cursor = data.Count - 1;
-			Shuffle();
-		}
-
-		public void AddRange(IEnumerable<T> collection)
-		{
-			IEnumerator<T> enumerator = collection.GetEnumerator();
-			while (enumerator.MoveNext())
-			{
-				Add(enumerator.Current);
-			}
-		}
-
 		public int Count => data.Count;
-
-
-		public void Clear() { data.Clear(); }
-
-		public bool Contains(T item) { return data.Contains(item); }
-
-		public void CopyTo(T[] array, int arrayIndex)
-		{
-			foreach (T item in data)
-			{
-				array.SetValue(item, arrayIndex);
-				arrayIndex = arrayIndex + 1;
-			}
-		}
-
-		public IEnumerator<T> GetEnumerator()
-		{
-			throw new System.NotImplementedException();
-		}
-
-		public int IndexOf(T item) { return data.IndexOf(item); }
-
-		public void Insert(int index, T item)
-		{
-			cursor = data.Count;
-			data.Insert(index, item);
-		}
-
-		public bool Remove(T item)
-		{
-			cursor = data.Count - 2;
-			return data.Remove(item);
-		}
-
-		public void RemoveAt(int index)
-		{
-			cursor = data.Count - 2;
-			data.RemoveAt(index);
-		}
-
-		IEnumerator IEnumerable.GetEnumerator(){ return data.GetEnumerator(); }
 	}
-
 }
