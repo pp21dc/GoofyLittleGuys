f438d6e792999e6c6e0d8073c118ff8f947fd683
RomanK <xt19uj@brocku.ca>
2025-02-12 17:28:18 -0500

    Slow Bug Fix
    - Fixed null ref bug that could occur at times with slow application

--- GoofyLittleGuys/Assets/_Scripts/Debuffs/Slow.cs
+++ GoofyLittleGuys/Assets/_Scripts/Debuffs/Slow.cs
@@ -7,29 +7,22 @@ public class Slow : Debuff
 {
 	private GameObject affectedGuy;
 	private GameObject instantiatedFX;
+	PlayerBody body;
+	LilGuyBase lilGuy;
 	public override void Init(float damage, float duration, float interval)
 	{
 		this.damage = damage;
 		this.duration = duration;
 		currentDuration = 0;
 		damageApplicationInterval = interval;
-
+		body = GetComponent<PlayerBody>();
+		lilGuy = (body == null) ? GetComponent<LilGuyBase>() : body.LilGuyTeam[0];
 		affectedGuy = gameObject;
 		StartCoroutine(Slowed(() => currentDuration, (value) => currentDuration = value));
 	}
 	private IEnumerator Slowed(Func<float> getCurrentDuration, Action<float> setCurrentDuration)
 	{
-		PlayerBody body = GetComponent<PlayerBody>();
-		LilGuyBase lilGuy = GetComponent<LilGuyBase>();
-		if (body != null)
-		{
-			body.LilGuyTeam[0].MoveSpeedModifier = Mathf.Max(lilGuy.MoveSpeedModifier + damage, 0.1f);
-		}
-		else
-		{
-
-			lilGuy.MoveSpeedModifier = Mathf.Max(lilGuy.MoveSpeedModifier +  damage, 0.1f);
-		}
+		lilGuy.MoveSpeedModifier = Mathf.Max(lilGuy.MoveSpeedModifier + damage, 0.1f);
 
 		while (getCurrentDuration() < duration)
 		{
@@ -44,15 +37,7 @@ public class Slow : Debuff
 	private void OnDestroy()
 	{
 
-		PlayerBody body = GetComponent<PlayerBody>();
-		LilGuyBase lilGuy = GetComponent<LilGuyBase>(); 
-		if (body != null)
-		{
-			body.LilGuyTeam[0].MoveSpeedModifier = Mathf.Max(lilGuy.MoveSpeedModifier - damage, 1f);
-		}
-		else
-		{
-			lilGuy.MoveSpeedModifier = Mathf.Max(lilGuy.MoveSpeedModifier - damage, 1f);
-		}
+		lilGuy.MoveSpeedModifier = Mathf.Max(lilGuy.MoveSpeedModifier - damage, 1f);
+
 	}
 }
