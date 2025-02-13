8b9deda9cab91916a16b73b695c23cce78f7327e
RomanK <xt19uj@brocku.ca>
2025-02-13 16:51:46 -0500

    SortOrderAdjustment Updates OnValidate
    - Added OnValidate sort order update
    - Added bool that enables/disables dynamic sort order adjustments

--- GoofyLittleGuys/Assets/_Scripts/Misc/SortOrderAdjustment.cs
+++ GoofyLittleGuys/Assets/_Scripts/Misc/SortOrderAdjustment.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class SortOrderAdjustment : MonoBehaviour
 {
     SpriteRenderer sprite;
+
+    [SerializeField] private bool movingObject = true;
     // Start is called before the first frame update
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+	private void OnValidate()
+	{
+		if (sprite == null) sprite = GetComponent<SpriteRenderer>();
+		sprite.sortingOrder = (int)-transform.position.z;
+	}
+
+	// Update is called once per frame
+	void Update()
     {
-        sprite.sortingOrder = (int) -transform.position.z;
+        if (movingObject) sprite.sortingOrder = (int) -transform.position.z;
     }
 }
