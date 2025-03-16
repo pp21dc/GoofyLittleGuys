f127fbe76612462ed52d4e3e5dce7d8a16c702d5
RomanK <xt19uj@brocku.ca>
2025-03-16 15:52:53 -0400

    Loading Screen Revamp
    - Loading Screen now does what's described in the document (uses a loading bar, animated mousecar that follows the bar, background cycles, slower load times currently only when loading the terrainwhitebox since we don't have a load into tutorial yet)

--- GoofyLittleGuys/Assets/_Scripts/Managers/LevelLoadManager.cs
+++ GoofyLittleGuys/Assets/_Scripts/Managers/LevelLoadManager.cs
@@ -39,11 +39,20 @@ namespace Managers
 		{
 			isLoadingLevel = true;
 			loadingScreen.gameObject.SetActive(true);
-			loadingScreen.LoadingBar.fillAmount = 0;
-			loadingScreen.LoadProgress.text = "0%";
-			yield return new WaitForSecondsRealtime(bufferTime);
 
-			// Unload all opened scenes (Not the persistent scene
+			float fakeProgress = 0f; // Fake progress value to control visual loading speed
+
+			if (levelName == "TerrainWhitebox")
+			{
+				// Change this condition to the transitions to/from tutorial screen. 
+				yield return new WaitForSecondsRealtime(bufferTime * 2);
+			}
+			else
+			{
+				yield return new WaitForSecondsRealtime(bufferTime);
+			}
+
+			// Unload previous scenes
 			if (currentLevelList.Count != 0)
 			{
 				for (int i = 0; i < currentLevelList.Count; i++)
@@ -51,25 +60,45 @@ namespace Managers
 				currentLevelList.Clear();
 			}
 
-			// Load new scene
+			// Start async scene loading
 			AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
+			asyncLoad.allowSceneActivation = false; // Prevent scene from loading instantly
+
 			while (!asyncLoad.isDone)
 			{
-				loadingScreen.LoadingBar.fillAmount = asyncLoad.progress;
-				loadingScreen.LoadProgress.text = asyncLoad.progress.ToString("0%");
-				// If we have a loading screen, we can update the slider progress, and pass asyncLoad.progress
+				// Gradually increase fake progress until it matches actual progress
+				while (fakeProgress < asyncLoad.progress)
+				{
+					fakeProgress = Mathf.MoveTowards(fakeProgress, asyncLoad.progress, Time.unscaledDeltaTime * 0.5f); // Adjust speed here
+					loadingScreen.LoadingBarOption2.value = fakeProgress;
+					loadingScreen.LoadProgress.text = (fakeProgress * 100f).ToString("0") + "%";
+
+					if (fakeProgress >= 0.5f)
+						loadingScreen.LoadingBackground.sprite = loadingScreen.LoadProgressBGs[1];
+					else
+						loadingScreen.LoadingBackground.sprite = loadingScreen.LoadProgressBGs[0];
+
+					yield return null;
+				}
+
+				// If scene is fully loaded but we haven't activated it yet, wait a bit more
+				if (asyncLoad.progress >= 0.9f)
+				{
+					yield return new WaitForSecondsRealtime(levelName == "TerrainWhitebox" ? 2.5f : 1.5f ); // Artificial delay to let bar reach 100% 
+																											// Change this condition to the transitions to/from tutorial screen as well.
+					asyncLoad.allowSceneActivation = true; // Finally activate the scene
+				}
+
 				yield return null;
 			}
-			//SceneManager.SetActiveScene(SceneManager.GetSceneByName(levelName));
 
 			currentLevelList.Add(levelName);
 
 			loadingScreen.gameObject.SetActive(false);
-			loadingScreen.LoadingBar.fillAmount = 0;
+			loadingScreen.LoadingBarOption2.value = 0;
 			loadingScreen.LoadProgress.text = "0%";
 			isLoadingLevel = false;
-
-			yield break;
 		}
+
 	}
 }
--- GoofyLittleGuys/Assets/_Scripts/Ui/Screens/LoadingScreen.cs
+++ GoofyLittleGuys/Assets/_Scripts/Ui/Screens/LoadingScreen.cs
@@ -7,8 +7,22 @@ using UnityEngine.UI;
 public class LoadingScreen : MonoBehaviour
 {
     [SerializeField] private Image loadingBar;
+    [SerializeField] private Image loadingBackground;
+    [SerializeField] private Slider loadingBarOption2;
     [SerializeField] private TMP_Text loadProgress;
 
+    [SerializeField] private Sprite[] loadingScreenBackgrounds;
+
     public Image LoadingBar => loadingBar;
+    public Slider LoadingBarOption2 => loadingBarOption2;
     public TMP_Text LoadProgress => loadProgress;
+    public Sprite[] LoadProgressBGs => loadingScreenBackgrounds;
+    public Image LoadingBackground => loadingBackground;
+
+	private void OnEnable()
+	{
+		LoadingBarOption2.value = 0;
+		LoadingBackground.sprite = LoadProgressBGs[0];
+		LoadProgress.text = "0%";
+	}
 }
