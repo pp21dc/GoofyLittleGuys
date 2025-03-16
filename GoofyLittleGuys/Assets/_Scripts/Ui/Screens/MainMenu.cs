9056fd056d02340988a1e09a0225af468ef6e426
RomanK <xt19uj@brocku.ca>
2025-03-16 17:02:04 -0400

    Menu Bug Fix
    - Fixed bug where main menu button presses would still register during loading screen, resulting in multiple character select loads if you spam accept.

--- GoofyLittleGuys/Assets/_Scripts/Ui/Screens/MainMenu.cs
+++ GoofyLittleGuys/Assets/_Scripts/Ui/Screens/MainMenu.cs
@@ -2,18 +2,33 @@ using Managers;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+	[SerializeField] private List<Button> buttons;
+
+
+	private void OnEnable()
+	{
+		ToggleButtons(true);
+	}
 	/// <summary>
 	/// Called when play is pressed.
 	/// </summary>
 	public void OnPlayButtonPressed()
 	{
+		ToggleButtons(false);
 		LevelLoadManager.Instance.LoadNewLevel("01_CharacterSelectMenu");
-
 	}
 
+	void ToggleButtons(bool active)
+	{
+		foreach (var button in buttons)
+		{
+			button.interactable = active;
+		}
+	}
 	/// <summary>
 	/// Called when Quit button is pressed. If we're in the editor, we just stop playmode.
 	/// </summary>
