27d366a617d2d5339ebcb62ec7645642c4f5d9c7
Allystarc <90292042+Allystarc@users.noreply.github.com>
2025-03-21 09:39:34 -0400

    phantaphant.cs error fix
    removed the line that was causing an error in phantaphant.cs

--- GoofyLittleGuys/Assets/_Scripts/LilGuys/Speed Type/Phantaphant.cs
+++ GoofyLittleGuys/Assets/_Scripts/LilGuys/Speed Type/Phantaphant.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class Phantaphant : SpeedType
 {
