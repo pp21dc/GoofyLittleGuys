7f06636f75dab2688052f37e9af164f6603c4969
Patrick Perry <93502969+pp21dc@users.noreply.github.com>
2025-04-01 20:50:16 -0400

    More color  blind/TTS stuff

---
+++ GoofyLittleGuys/Assets/_Scripts/TextToSpeech/TextToSpeechObject.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextToSpeechObject : MonoBehaviour
+{
+    [SerializeField] private AudioClip clip;
+
+    public void Read(AudioClip clip)
+    {
+        TextToSpeech.Instance.TTS(clip);
+    }
+}
