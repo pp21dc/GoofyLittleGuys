8be4ab24f4a1e3ec68c465b8fc619b212e77b010
FlockOfMeese <90285111+FlockOfMeese@users.noreply.github.com>
2025-04-06 20:32:47 -0400

    Water Sound Re-Implemented & Tbox SFX change
    Tbox special punch was changed, and the water audio source now works in a better way.

--- GoofyLittleGuys/Assets/_Scripts/Environment/WaterAudioSource.cs
+++ GoofyLittleGuys/Assets/_Scripts/Environment/WaterAudioSource.cs
@@ -6,14 +6,68 @@ public class WaterAudioSource : MonoBehaviour
 {
     private bool isPlaying = false;
     [SerializeField] private AudioSource waterAudioSource;
+    //[SerializeField] private BoxCollider quietZone; //Larger trigger that will play the sound at half volume
+    [SerializeField] private BoxCollider loudZone; //Smaller trigger that plays sound at full volume
+
+    private int numPlayersNear = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (!isPlaying)
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if it wasn't a player that entered, do nothing.
+        var playerHit = other.gameObject.GetComponent<LilGuyBase>()?.PlayerOwner;
+        if (playerHit == null) return;
+
+        //if a player entered, increment numPlayersNear.
+        numPlayersNear++;
+
+        if (!isPlaying && numPlayersNear == 1)
         {
-            Managers.AudioManager.Instance.PlaySfx("Water", waterAudioSource);
             isPlaying = true;
+            PlayLoopingSound("Water");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //if it wasn't a player that left, do nothing.
+        var playerHit = other.gameObject.GetComponent<LilGuyBase>()?.PlayerOwner;
+        if (playerHit == null) return;
+
+        //if a player left the trigger, decrease numPlayersNear, setting it to at least zero.
+        numPlayersNear--;
+        if (numPlayersNear < 0) { numPlayersNear = 0; }
+
+        //if no players are nearby, stop playing the sound and update isPlaying to false.
+        if (isPlaying && numPlayersNear <= 0) 
+        {
+            StopLoopingSound();
+            isPlaying = false;
+        }
+    }
+
+    //Changes this source to loop and plays a sound until StopLoopingSound is called
+    private void PlayLoopingSound(string key)
+    {
+        if (waterAudioSource.loop == false)
+        {
+            waterAudioSource.loop = true;
+        }
+        Managers.AudioManager.Instance.PlaySfx(key, waterAudioSource);
+    }
+
+    //Changes this source to not loop and stops whatever it is currently playing
+    private void StopLoopingSound()
+    {
+        if (waterAudioSource.loop == true && waterAudioSource.isPlaying)
+        {
+            waterAudioSource.loop = false;
+            waterAudioSource.Stop();
         }
     }
 }
